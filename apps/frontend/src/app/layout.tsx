af033f0e5cc421d221cd0d44f822672df2d54141
Atef Kai Benothman <atefkbenothman@gmail.com>
2025-06-14 00:53:14 -0700

    chore(frontend): use geist mono font

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -10,12 +10,11 @@ import { DarkModeProvider } from "@/providers/dark-mode-provider"
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server"
 import { asyncTryCatch } from "@dojo/utils"
 import { Analytics } from "@vercel/analytics/next"
+import { GeistMono } from "geist/font/mono"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { cookies } from "next/headers"
 import { Toaster } from "sonner"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Dojo",
@@ -82,7 +81,7 @@ export default async function RootLayout({
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⛩️</text></svg>"
           />
         </head>
-        <body className={`antialiased ${inter.className}`}>
+        <body className={`antialiased ${GeistMono.className}`}>
           <ConvexClientProvider>
             <DarkModeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
               <SessionInitializer>
